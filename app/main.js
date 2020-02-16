b3134cae81457509fe12e3f1fdb00923dc1cd59b
Subb98 <subb98@yandex.ru>
2020-02-16 19:25:01 +0500

    Update webpack

--- app/main.js
+++ app/main.js
@@ -3,6 +3,6 @@ require('./main.css');
 function loadContent() {
   document.getElementById('title').innerText = 'Hello webpack!';
   document.getElementById('text').innerText = 'Created with';
-};
+}
 
 document.body.onload = loadContent();
